cb2531281659f5a790916110a5c9c1065e52ba11
thefakezach <zach@takedetroit.com>
2025-09-27 23:30:46 -0400

    deploy 1st

--- astro.config.ts
+++ astro.config.ts
@@ -1,12 +1,15 @@
 import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
+import netlify from '@astrojs/netlify';
 
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+  output: 'server',
+  adapter: netlify(),
 
   vite: {
     plugins: [tailwindcss()]
